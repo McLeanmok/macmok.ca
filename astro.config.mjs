6c0550c5f490a1007fe7efe66209714612daf8f6
McLeanmok <165439265+McLeanmok@users.noreply.github.com>
2026-01-28 12:23:17 -0800

    Analytics Intergration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,18 @@ import purgecss from 'astro-purgecss';
 
 import { defineConfig } from 'astro/config';
 
+import partytown from '@astrojs/partytown';
+
 export default defineConfig({
   // Add purgecss support to Astro
-  integrations: [
-    purgecss({
-      fontFace: true,
-     variables: true,
-    }),
-    
-  ],
+  integrations: [purgecss({
+    fontFace: true,
+   variables: true,
+  }), partytown({
+    config: {
+              forward: ["dataLayer.push"],
+            },
+  })],
   outDir: 'build',
   build: {
     format: 'preserve',
