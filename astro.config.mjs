bd6ddc0a2390e172981b907fd2198cbf6a65510e
McLeanmok <165439265+McLeanmok@users.noreply.github.com>
2026-06-23 19:56:32 -0700

    Cloudflare ready to go on beta branch

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import purgecss from 'astro-purgecss';
 import partytown from '@astrojs/partytown';
 import icon from 'astro-icon';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   integrations: [
     purgecss({
@@ -18,12 +20,17 @@ export default defineConfig({
       iconDir: "src/icons",
     }),
   ],
+
   outDir: 'build',
+
   build: {
     format: 'preserve',
     inlineStylesheets: 'never'
   },
+
   assets: {
     basePath: "/",
-  }
-}); 
+  },
+
+  adapter: cloudflare()
+});
